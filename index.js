10530cb30a40c2bef3111234f979a47223385189
github username <sharnellgrant@gmail.com>
2021-02-05 20:48:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+/* Enter the code to remove the node element here */
+main.remove()
+
+const newHeader = document.createElement("h1"); 
+newHeader.id = "victory"
+newHeader.innerHTML = "is the champion"
